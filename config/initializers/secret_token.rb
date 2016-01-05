a2fb0279695af2daf9cd59958270a8530173331c
KevinPiaskowski <vagrant@rails-dev-box>
2016-01-05 19:44:05 +0000

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PingPongTrackerApp::Application.config.secret_key_base = '7174f1b97c0090af474e0166d233011eaf92649d14d6573e74b8753be7d243a182780ce328325908e464587a7b3220769fa6ea8876336ba5fde3adce5856a4f3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PingPongTrackerApp::Application.config.session_store :cookie_store, key: '_ping_pong_tracker_app_session'
