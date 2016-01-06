331a1c12c0e5a7152b1e8d473b4a5b8cea3e926e
KevinPiaskowski <vagrant@rails-dev-box>
2016-01-06 14:53:35 +0000

    Added basic view for homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 PingPongTrackerApp::Application.routes.draw do
+  get 'homepage/home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- test/controllers/homepage_controller_test.rb
+++ test/controllers/homepage_controller_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class HomepageControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get home" do
+    get :home
+    assert_response :success
+    assert_select "title", "Home | Ping Pong Tracker Application"
+  end
 end
